8574c8911f83bf62787f580a63bb16b97a4ebddc
vsiyer91 <vsiyer91@gmail.com>
2016-02-06 17:13:08 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("navigator").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='coursework.html'>Coursework</a></li>" +
+"<li><a href='project1.html'>WMS</a></li>" +
+"<li><a href='greenhouse.html'>Automated Terrace Farm</a></li>" +
+"<li><a href='project2.html'>RnD Project</a></li>" +
+"<li><a href='3dworld.html'>3D Designs</a></li>" +
+//"<li><a href='hacks.html'>Hacks</a></li>" +
+//"<li><a href='places.html'>Places</a></li>" +
+"</ul>";
